4f217dca4302625290316047ff6121fa3ebfdba9
Andrew Medina Condo <henry.medina@tecsup.edu.pe>
2022-11-13 08:59:57 -0500

    add change

--- static/script/controllerCv.js
+++ static/script/controllerCv.js
@@ -7,11 +7,30 @@ function getData(){
     const email=document.getElementById('emailF').value;
     const ocupation=document.getElementById('ocupationF').value;
     const telefono=document.getElementById('telefonoF').value;
-    // Get Data form Select [Options]
+    
+    // Get Data form Country
     var country=document.getElementById('countryA');
     var index=country.value;
     var textCountry=country.options[country.selectedIndex].text;
+    // console.log(index);
+    console.log(country);
     console.log(index);
     console.log(textCountry);
-    console.log("Lol")
+    // console.log(textCountry);
 }
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict'
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
